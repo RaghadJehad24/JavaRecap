b81b7920a5e7a7b471f486ffbcb37a1fd422db82
raghadjehad24 <ragood.200024@gmail.com>
2025-06-07 17:35:44 +0300

    initial  commit

---
+++ src/main/java/com/learn/recap/RecapJavaApplication.java
@@ -0,0 +1,13 @@
+package com.learn.recap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecapJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/recap/RecapJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.recap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
